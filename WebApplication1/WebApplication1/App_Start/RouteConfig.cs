d37486e0d010a6922b16751c78122583cf00b7da
michaelxuan <michaelxuan@hotmail.com>
2018-08-19 11:06:41 +0800

    first commit

---
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebApplication1.Controllers; // remember to import this name space so sub controller can be used, i.e. PostsController
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            // Gets each sub controller where the sub controllers of area definition  their namespace value
+            // And then we can use it to distinguish the different project controller and area controller...
+            var namespaces = new[ ] { typeof (PostsController).Namespace};
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication1.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName { get { return "admin"; } } 
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
